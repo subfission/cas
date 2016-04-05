fa09693449fa06ce09d44a95733d0d0a40e29ba7
Zach :(){ :|: & } <subfission@users.noreply.github.com>
2016-04-05 04:22:01 -0700

    updated to master
    * Typo (non-breaking)
    
    Fixed typo
    
    * Typo discovery
    
    Typo discovered and fixed in 8b824de.
    
    * Updated middleware
    
    Found rare issue where session may become desynchronized with authentication.  Stripped out the Laravel auth for use of sessions.

--- src/Subfission/Cas/Middleware/CASAuth.php
+++ src/Subfission/Cas/Middleware/CASAuth.php
@@ -24,7 +24,7 @@ class CASAuth
      */
     public function handle($request, Closure $next)
     {
-        if ($this->auth->guest() || ! session()->has('cas_user')) {
+        if ( ! session('cas_user') ) {
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
             }
@@ -33,7 +33,7 @@ class CASAuth
             }
             // We setup CAS here to reduce the amount of objects we need to build at runtime.  This
             // way, we only create the CAS calls only if the user has not yet authenticated.
-            session()->put('cas_user', $this->cas->User());
+            session()->put('cas_user', $this->cas->user());
         }
 
         return $next($request);
