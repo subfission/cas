2c46a236e27bb686b1c6a074720a142284ff93fb
flartet <florent.lartet@univ-tlse2.fr>
2017-01-11 16:16:24 +0100

    internal login user if set in config
    Ability to login the user in Laravel, useful for a lot of packages that need to have a real Laravel session (ex : permissions / roles)

--- src/Subfission/Cas/Middleware/CASAuth.php
+++ src/Subfission/Cas/Middleware/CASAuth.php
@@ -28,6 +28,12 @@ class CASAuth
         {
             // Store the user credentials in a Laravel managed session
             session()->put('cas_user', $this->cas->user());
+            // manage the internal login process if the cas user is found in internal db
+            $config = $this->cas->getConfig();
+            $user = User::where($config['user_cas_attribute'], $this->cas->user())->first();
+            if (($config['use_laravel_sessions']) && (isset($user))) {
+                Auth::login($user);
+            }
         } else {
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
