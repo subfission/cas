6adb5b127f3dc42777715b77ddd567a630300298
Zach :(){ :|: & } <subfission@users.noreply.github.com>
2017-03-06 12:37:55 -0700

    Fixed missing prefix
    Fixed a config prefix that prevented access to some env variables

--- src/config/config.php
+++ src/config/config.php
@@ -68,7 +68,7 @@ return [
     |
     | VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL
     */
-    'validation'          => env('CAS_VALIDATION', ''),
+    'cas_validation'          => env('CAS_VALIDATION', ''),
 
     /*
     |--------------------------------------------------------------------------
@@ -77,7 +77,7 @@ return [
     | Path to the CA certificate file.  For production use set
     | the CA certificate that is the issuer of the cert
     */
-    'cert'                => env('CAS_CERT', ''),
+    'cas_cert'                => env('CAS_CERT', ''),
 
     /*
     |--------------------------------------------------------------------------
