652066aeee3a43f4a26ad56c2c1cccbd7f61a197
Antonin <antonin.guyot@icloud.com>
2020-10-20 11:40:22 +0200

    Added PHPDoc methods to the facade

--- src/Subfission/Cas/Facades/Cas.php
+++ src/Subfission/Cas/Facades/Cas.php
@@ -1,6 +1,24 @@
 <?php namespace Subfission\Cas\Facades;
 
 use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static bool authenticate()
+ * @method static array getConfig()
+ * @method static string user()
+ * @method static string getCurrentUser()
+ * @method static mixed getAttribute($key)
+ * @method static bool hasAttribute($key)
+ * @method static logout($url = '', $service = '')
+ * @method static logoutWithUrl($url)
+ * @method static mixed getAttributes()
+ * @method static boolean isAuthenticated()
+ * @method static boolean checkAuthentication()
+ * @method static bool isMasquerading()
+ * @method static setAttributes(array $attr)
+ *
+ * @see \Subfission\Cas\CasManager
+ */
 class Cas extends Facade {
     /**
      * Get the registered name of the component.
