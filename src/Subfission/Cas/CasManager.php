7ca6cc7662917d2c63eab445ca003a3387461d6e
Dirk Tepe <tepeds@miamioh.edu>
2021-02-19 07:28:05 -0500

    Constrain phpcas package to 1.3.8
    in response to a breaking change introduced in 1.3.9 (see https://github.com/apereo/phpCAS/issues/371)

--- src/Subfission/Cas/CasManager.php
+++ src/Subfission/Cas/CasManager.php
@@ -26,12 +26,7 @@ class CasManager {
 	public function __construct( array $config ) {
 		$this->parseConfig( $config );
 		if ( $this->config['cas_debug'] === true ) {
-			try {
-				phpCAS::setDebug();
-			} catch (ErrorException) {
-				// Fix for depreciation of setDebug
-				phpCAS::setLogger();
-			}
+            phpCAS::setDebug();
 			phpCAS::log( 'Loaded configuration:' . PHP_EOL
 			             . serialize( $config ) );
 		} else {
